fc65e263f9a847a9e1a150758b0d3e82c91fb703
marharita08 <rozgon.margarita08@gmail.com>
2026-03-26 13:43:21 +0200

    update mongoose.ts

--- src/lib/db/mongoose.ts
+++ src/lib/db/mongoose.ts
@@ -2,16 +2,31 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
+type MongooseGlobal = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
 declare global {
-  var mongoose: { conn: mongoose.Connection | null };
+  var _mongoose: MongooseGlobal | undefined;
 }
 
-const cached = globalThis.mongoose ?? { conn: null };
-globalThis.mongoose = cached;
+const cached = global._mongoose ?? {
+  conn: null,
+  promise: null,
+};
+
+global._mongoose = cached;
 
 export async function connectDB() {
   if (cached.conn) return cached.conn;
-  const { connection } = await mongoose.connect(MONGODB_URI);
-  cached.conn = connection;
-  return connection;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
